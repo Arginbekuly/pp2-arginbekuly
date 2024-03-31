5e1e275def7b14b351dbaf4bda78c0adae12faf7
Arginbekuly <kzantureev@gmail.com>
2024-04-01 02:53:16 +0500

    lab8

--- lab 8/paint/Paint.py
+++ lab 8/paint/Paint.py
@@ -1,125 +1,66 @@
 import pygame
+import os
+pygame.init()
+os.chdir(r'C:\Users\argyn\Desktop\pp2\lab 8\paint')
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((640, 480))
-    clock = pygame.time.Clock()
-    
-    radius = 15
-    x = 0
-    y = 0
-    mode = 'blue'
-    points = []
-    #Boolean to eraser and draw and circle and rect 
-    draw=True
-    cirrect=True
-    
-    while True:
-        
-        pressed = pygame.key.get_pressed()
-        
-        alt_held = pressed[pygame.K_LALT] or pressed[pygame.K_RALT]
-        ctrl_held = pressed[pygame.K_LCTRL] or pressed[pygame.K_RCTRL]
-        
-        for event in pygame.event.get():
-            
-            # determin if X was clicked, or Ctrl+W or Alt+F4 was used
-            if event.type == pygame.QUIT:
-                return
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w and ctrl_held:
-                    return
-                if event.key == pygame.K_F4 and alt_held:
-                    return
-                if event.key == pygame.K_ESCAPE:
-                    return
-            
-                # determine if a letter key was pressed
-                if event.key == pygame.K_r:
-                    mode = 'red'
-                elif event.key == pygame.K_g:
-                    mode = 'green'
-                elif event.key == pygame.K_b:
-                    mode = 'blue'
-                #turn on Eraser and drawer
-                if event.key == pygame.K_SPACE and draw == False:
-                    draw=True
-                    points = []
-                elif event.key == pygame.K_SPACE and draw == True:
-                    draw=False
-                    points = []
-                #turn on Rect and Circle   
-                if event.key == pygame.K_z and cirrect == False:
-                    cirrect=True
-                elif event.key == pygame.K_z and cirrect == True:
-                    cirrect=False                  
-            
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1: # left click grows radius
-                    radius = min(200, radius + 1)
-                elif event.button == 3: # right click shrinks radius
-                    radius = max(1, radius - 1)
-            
-            if event.type == pygame.MOUSEMOTION:
-                # if mouse moved, add point to list
-                position = event.pos
-                points = points + [position]
-                points = points[-256:]
-            
-                
-        
-        # draw all points
-        i = 0
-        while i < len(points) - 1 and draw==True:
-            drawLineBetween(screen, i, points[i], points[i + 1], radius, mode,cirrect)
-            i += 1
-        #eraser draw all points
-        while i < len(points) - 1 and draw==False:
-            Eraser(screen, i, points[i], points[i + 1], radius, mode,cirrect)
-            i += 1
-        
-        pygame.display.flip()
-        
-        clock.tick(60)
+screen = pygame.display.set_mode((1080, 900))
+clock = pygame.time.Clock()
 
-def drawLineBetween(screen, index, start, end, width, color_mode,cirrect):
-    c1 = max(0, min(255, 2 * index - 256))
-    c2 = max(0, min(255, 2 * index))
-    
-    if color_mode == 'blue':
-        color = (c1, c1, c2)
-    elif color_mode == 'red':
-        color = (c2, c1, c1)
-    elif color_mode == 'green':
-        color = (c1, c2, c1)
-    
-    dx = start[0] - end[0]
-    dy = start[1] - end[1]
-    iterations = max(abs(dx), abs(dy))
-    
-    for i in range(iterations):
-        progress = 1.0 * i / iterations
-        aprogress = 1 - progress
-        x = int(aprogress * start[0] + progress * end[0])
-        y = int(aprogress * start[1] + progress * end[1])
-        if cirrect==True:
-            pygame.draw.circle(screen, color, (x, y), width)
+RED = (230, 0, 0)
+GREEN = (0, 230, 0)
+BLUE = (0, 0, 230)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+colors = [RED, GREEN, BLUE]
+color = WHITE
+
+eraser = pygame.image.load('images//eraser.png')
+eraser = pygame.transform.scale(eraser, (70, 70))
+
+def draw_rect(index):
+    pygame.draw.rect(screen, colors[index], (index*40, 0, 40, 40))
+
+def pick_color():
+    click = pygame.mouse.get_pressed()
+    x, y = pygame.mouse.get_pos()
+    if click[0]:
+        if 0<=x<=40 and 0<=y<=40:
+            return RED
+        elif 40<x<=80 and 0<=y<=40:
+            return GREEN
+        elif 80<x<=120 and 0<=y<=40:
+            return BLUE
+        elif 1010<=x<=1080 and 0<=y<=40:
+            return BLACK
+        elif 130<=x<=170 and 0 <= y <= 40:
+            return "rect"
+    return color
+
+def painting(color):
+    click = pygame.mouse.get_pressed()
+    x, y = pygame.mouse.get_pos()
+    if click[0]:
+        if color != 'rect':
+            pygame.draw.circle(screen, color, (x, y), 27)
         else:
-            pygame.draw.rect(screen,color,(x-width,y-width,2*width,2*width))   
-# Eraser button space
-def Eraser(screen, index, start, end, width, color_mode,cirrect):
-    dx = start[0] - end[0]
-    dy = start[1] - end[1]
-    iterations = max(abs(dx), abs(dy))
-    
-    for i in range(iterations):
-        progress = 1.0 * i / iterations
-        aprogress = 1 - progress
-        x = int(aprogress * start[0] + progress * end[0])
-        y = int(aprogress * start[1] + progress * end[1])
-        if cirrect==True:
-            pygame.draw.circle(screen, (0,0,0), (x, y), width)
-        else:
-            pygame.draw.rect(screen,(0,0,0),(x-width,y-width,2*width,2*width))        
+            pygame.draw.rect(screen, WHITE, (x, y, 40, 40), 4)
 
-main()
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    
+    for i in range(len(colors)):
+        draw_rect(i)
+    screen.blit(eraser, (1010, 0))
+    pygame.draw.rect(screen, WHITE, (130, 0, 40, 40), 4)
+
+    color = pick_color()
+    painting(color)
+
+
+    clock.tick(370)
+    pygame.display.update()
